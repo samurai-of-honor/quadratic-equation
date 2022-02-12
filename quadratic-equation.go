f81529dce0ea072fd3981e3f9951f6546517e624
Pasha Baran <pashabaran1207@gmail.com>
2022-02-13 00:25:32 +0200

    Adding file mode. Formatting fix
    In this commit, I added the first version of the program in file mode (arguments are not read from the console but from the file).  A file with arguments args.txt was also added in which a, b and c are still being read from a new line (perhaps this version of the file will remain). Source code formatted with go fmt.
    
    The code that waits for "q" to exit has been added for future reference when the program is compiled and run on the command line.
    
    The problem with error handling in interactive mode is still relevant. Also I need to implement error handling when a wrong file is entered.

--- quadratic-equation.go
+++ quadratic-equation.go
@@ -1,32 +1,77 @@
 package main
-import . "fmt"
-import "math"
+
+import (
+	"bufio"
+	. "fmt"
+	"math"
+	"os"
+	"strconv"
+)
 
 func main() {
-  var a, b, c float64
-  Print("a = ")
-  Scan(&a)
-  Print("b = ")
-  Scan(&b)
-  Print("c = ")
-  Scan(&c)
-  Printf("Equation is: (%.1f) x^2 + (%.1f) x + (%.1f) = 0\n", a, b, c)
-  equation(a, b, c)
+	var a, b, c float64
+	var argumentIndex int
+
+	checkArg := os.Args[1]
+	if checkArg == "args.txt" {
+		file, err := os.Open("args.txt")
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+
+		str := bufio.NewScanner(file)
+		for str.Scan() {
+			number, err := strconv.ParseFloat(str.Text(), 64)
+			if err != nil {
+				panic(err)
+			}
+			switch {
+			case argumentIndex == 0:
+				a = number
+			case argumentIndex == 1:
+				b = number
+			case argumentIndex == 2:
+				c = number
+			}
+			argumentIndex++
+		}
+	} else {
+		Print("a = ")
+		Scan(&a)
+		Print("b = ")
+		Scan(&b)
+		Print("c = ")
+		Scan(&c)
+	}
+
+	Printf("Equation is: (%.1f) x^2 + (%.1f) x + (%.1f) = 0\n", a, b, c)
+	equation(a, b, c)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		exit := scanner.Text()
+		if exit == "q" {
+			break
+		} else {
+			Println("Enter 'q' to quit")
+		}
+	}
 }
 
 func equation(a, b, c float64) {
-  D := b * b - 4 * a * c
-  square := math.Sqrt(D)
+	D := b*b - 4*a*c
+	square := math.Sqrt(D)
 
-  switch {
-  case D < 0:
-    Println("There are 0 roots")
-  case D == 0:
-    Println("There are 1 roots")
-    Println("x1 =", -b / ( 2 * a ))
-  case D > 0:
-    Println("There are 2 roots")
-    Println("x1 =", (-b + square) / ( 2 * a ))
-    Println("x2 =", (-b - square) / ( 2 * a ))
-  }
+	switch {
+	case D < 0:
+		Println("There are 0 roots")
+	case D == 0:
+		Println("There are 1 roots")
+		Println("x1 =", -b/(2*a))
+	case D > 0:
+		Println("There are 2 roots")
+		Println("x1 =", (-b+square)/(2*a))
+		Println("x2 =", (-b-square)/(2*a))
+	}
 }
