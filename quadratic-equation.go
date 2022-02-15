fba869fd88a0e9bcec07af478c400a053b55baa7
Pasha Baran <pashabaran1207@gmail.com>
2022-02-15 19:01:31 +0200

    Implementing error processing
    In this commit I fixed reading arguments from a file (now you can specify any file, not just args.txt).
    
    Also, a function has been added to enter arguments in interactive mode, which contains the processing of incorrect input.

--- quadratic-equation.go
+++ quadratic-equation.go
@@ -12,9 +12,9 @@ func main() {
 	var a, b, c float64
 	var argumentIndex int
 
-	checkArg := os.Args[1]
-	if checkArg == "args.txt" {
-		file, err := os.Open("args.txt")
+	checkArg := os.Args
+	if len(checkArg) > 1 {
+		file, err := os.Open(checkArg[1])
 		if err != nil {
 			panic(err)
 		}
@@ -37,17 +37,14 @@ func main() {
 			argumentIndex++
 		}
 	} else {
-		Print("a = ")
-		Scan(&a)
-		Print("b = ")
-		Scan(&b)
-		Print("c = ")
-		Scan(&c)
+		a = inputArg("a")
+		b = inputArg("b")
+		c = inputArg("c")
 	}
-
 	Printf("Equation is: (%.1f) x^2 + (%.1f) x + (%.1f) = 0\n", a, b, c)
 	equation(a, b, c)
 
+	Println("Enter 'q' to quit")
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		exit := scanner.Text()
@@ -55,6 +52,29 @@ func main() {
 			break
 		} else {
 			Println("Enter 'q' to quit")
+		}
+	}
+}
+
+func inputArg(arg string) float64 {
+	for {
+		switch {
+		case arg == "a":
+			Print("a = ")
+		case arg == "b":
+			Print("b = ")
+		case arg == "c":
+			Print("c = ")
+		}
+		str, _, readerErr := bufio.NewReader(os.Stdin).ReadLine()
+		if readerErr != nil {
+			panic(readerErr)
+		}
+		result, convertErr := strconv.ParseFloat(string(str), 64)
+		if convertErr != nil {
+			Println(convertErr)
+		} else {
+			return result
 		}
 	}
 }
